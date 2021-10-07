498d012aade5d6aa5a4365be8c24f5242166d5c4
Chris Diehl <cultclassik@gmail.com>
2021-10-07 11:16:05 -0500

    test

--- jump_box.tf
+++ jump_box.tf
@@ -33,7 +33,7 @@ resource "azurerm_network_interface" "vm" {
 # }
 
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                            = "jump-box"
+  name                            = "jump-box1"
   location                        = azurerm_resource_group.aks.location
   resource_group_name             = azurerm_resource_group.aks.name
   size                            = "Standard_B1LS"
